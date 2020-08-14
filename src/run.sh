719b472211a18801f5c5be004a90ee196dca1acf
nye0 <yeningrong@csu.edu.cn>
2020-08-14 22:28:51 +0800

    init

---
+++ src/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+SUBJ=$1
+SUBJ_DIR=$2
+surf=$3
+label=$4
+resultroot=$5
+#pial_name=`basename $pial`
+matlab=/Applications/MATLAB_R2016b.app/bin//matlab
+
+for h in lh rh; do
+	mris_convert $SUBJ_DIR/$SUBJ/surf/${h}.${surf} $resultroot/${h}.${surf}.asc
+	asc=$resultroot/${h}.${surf}.asc
+	srf=$resultroot/${h}.${surf}.srf
+	obj=$resultroot/${h}.${surf}.obj
+	mv $asc $srf
+	src/srf2obj $srf > $obj
+
+        mris_label2annot        --s $SUBJ \
+                                --h $h \
+                                --sd $SUBJ_DIR \
+                                --ctab $SUBJ_DIR/$SUBJ/label/aparc.annot.ctab \
+                                --a $label \
+                                --l $SUBJ_DIR/$SUBJ/label/${h}.${label}.label \
+				--l $SUBJ_DIR/$SUBJ/label/${h}.BA1_exvivo.label 
+        annot_use=$resultroot/${h}.${label}.annot
+	mv $SUBJ_DIR/$SUBJ/label/${h}.${label}.annot $annot_use
+	$matlab -nodesktop -nosplash -r "annot2dpv $annot_use ${annot_use}.dpv;splitsrf $srf ${annot_use}.dpv $resultroot/${h}.pial.${label};exit;"
+	for n_srf in `ls $resultroot/${h}.pial.${label}*.srf`; do
+		n_obj=${n_srf%%srf}obj
+		src/srf2obj $n_srf > $n_obj
+	done
+done
+
+
